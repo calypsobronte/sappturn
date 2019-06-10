45022f801faccc411fd9a85ef9d0e896bf60f8f5
Lina Maria Montaño Ramirez | calypso Bronte <codelinamaria@gmail.com>
2019-06-09 22:46:25 -0500

    update src/index

---
+++ build/precache-manifest.6c260b78a11a6c43407b63f85154c517.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "209b9681f0c0f50b3758b5c275142c73",
+    "url": "/calypsobronte/sappturn/index.html"
+  },
+  {
+    "revision": "ea9ebdfcf8b33323ac22",
+    "url": "/calypsobronte/sappturn/static/css/main.ee553b7c.chunk.css"
+  },
+  {
+    "revision": "0cd422167401ba838501",
+    "url": "/calypsobronte/sappturn/static/js/2.0e993634.chunk.js"
+  },
+  {
+    "revision": "ea9ebdfcf8b33323ac22",
+    "url": "/calypsobronte/sappturn/static/js/main.11b03fb0.chunk.js"
+  },
+  {
+    "revision": "915b02c2979f893fe7c4",
+    "url": "/calypsobronte/sappturn/static/js/runtime~main.19485780.js"
+  },
+  {
+    "revision": "822126e13b9ee369b3b49488f22d7b87",
+    "url": "/calypsobronte/sappturn/static/media/sappturn_logo_v2.822126e1.jpeg"
+  }
+]);
